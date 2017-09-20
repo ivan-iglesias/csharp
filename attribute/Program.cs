fcc2eb6665f7be06264ad2d13f8b40fa6c1250b5
ivan-iglesias <iiglesiasbilbao@gmail.com>
2017-09-20 19:07:11 +0200

    Attribute

---
+++ attribute/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+
+// An attribute is a declarative tag that is used to convey information (metadata)
+// to runtime about the behaviors of various elements like classes, methods, structures,
+// enumerators, assemblies etc. in your program.
+
+namespace attribute
+{
+    public class Myclass
+    {
+        [Conditional("DEBUG")]
+        public static void Message(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+
+    class Program
+    {
+        #if DEBUG
+            public const String ENVIRONMENT = "Development";
+        #else
+            public const String ENVIRONMENT = "Production";
+        #endif
+
+        static void FunctionNew()
+        {
+            Myclass.Message("In new function.");
+        }
+
+        [Obsolete("Don't use FunctionOld, use FunctionNew instead", false)]
+        static void FunctionOld()
+        {
+            Myclass.Message("In old function.");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ENVIRONMENT);
+            Myclass.Message("In Main function.");
+            FunctionNew();
+            FunctionOld();
+            Console.ReadKey();
+        }
+    }
+}
